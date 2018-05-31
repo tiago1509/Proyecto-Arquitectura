c2615b2ad95e1eefc9fce5ece86151eb3a5d8448
Santiago Campuzano <tiago150995@gmail.com>
2018-05-30 22:05:45 -0500

    Front

--- Arquitectura/app.tsx
+++ Arquitectura/app.tsx
@@ -6,33 +6,80 @@ var ReactDOM = require('react-dom');
 class Head extends React.Component {
     render() {
         return (
-            <div>
-                <form>
-                    <h1>Renderizando en React!</h1>
-
-                    <label for="id">Last Name</label>
-                    <input type="text" id="id" name="idUser" placeholder="Escriba la cédula.."></input>
-
-                    <label for="name">Nombre usuarios</label>
-                    <input type="text" id="name" name="names" placeholder="Escriba el nombre..."></input>
-
-                    <label for="cuadrante">Cuadrante</label>
-                    <select id="cuadrante" name="cuadrante">
-                        <option value="norte">Norte</option>
-                        <option value="sur">Sur</option>
-                        <option value="oriente">Oriente</option>
-                        <option value="occidente">Occidente</option>
-                    </select>
-
-                    <label for="subject">Reporte</label>
-                    <textarea id="subject" name="subject" placeholder="Escriba el detalle del problema..." style="height:200px"></textarea>
-
-                    <input type="submit" value="Enviar"></input>
-                </form>
+          <div class="login-page">
+            <div class="form">
+              <form class="login-form">
+                <input type="text" placeholder="Cédula"/>
+                <input type="password" placeholder="Contraseña"/>
+                <button>Entrar</button>
+                <p class="message">Crea una cuenta <a href="#">aquí</a></p>
+              </form>
             </div>
-
+          </div>
         );
     }
 }
 
-ReactDOM.render(<Head />, document.getElementById('root'));
+class Registro extends React.Component {
+    render() {
+        return (
+          <div>
+              <form>
+                  <center>
+                      <h1>Registro de Usuarios</h1>
+                  </center>
+
+                  <label for="id">Cédula del usuario</label>
+                  <input type="text" id="id" name="idUser" placeholder="Escriba su cédula de ciudadanía.."></input>
+
+                  <label for="name">Nombres y apellidos</label>
+                  <input type="text" id="name" name="names" placeholder="Escriba su nombre completo..."></input>
+
+                  <label for="pas">Contraseña</label>
+                  <input type="text" id="name" name="names" placeholder="Escriba la nueva contraseña..."></input>
+
+                  <label for="name">Email</label>
+                  <input type="text" id="name" name="names" placeholder="Escriba su email..."></input>
+
+                  <label for="name">Dirección</label>
+                  <input type="text" id="name" name="names" placeholder="Escriba su dirección..."></input>
+
+                  <label for="name">Teléfono</label>
+                  <input type="text" id="name" name="names" placeholder="Escriba su teléfono..."></input>
+
+                  <center>
+                      <input type="submit" value="Registrarse"></input>
+                  </center>
+              </form>
+          </div>
+          );
+        }
+}
+class Reporte extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ingreso de Reporte</h1>
+                <label for="id">Cédula del usuario</label>
+                <input type="text" id="id" name="idUser" placeholder="Escriba la cédula.."></input>
+                <label for="name">Nombre usuario</label>
+                <input type="text" id="name" name="names" placeholder="Escriba el nombre..."></input>
+                <label for="cuadrante">Cuadrante</label>
+                <select id="cuadrante" name="cuadrante">
+                    <option value="norte">Norte</option>
+                    <option value="sur">Sur</option>
+                    <option value="oriente">Oriente</option>
+                    <option value="occidente">Occidente</option>
+                </select>
+                <label for="subject">Reporte</label>
+                <textarea id="subject" name="subject" placeholder="Escriba el detalle del problema..." style="height:200px"></textarea>
+                <input type="submit" value="Enviar"></input>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<Head />, document.getElementById('log'));
+ReactDOM.render(<Reporte />, document.getElementById('rep'));
+ReactDOM.render(<Registro />, document.getElementById('reg'));
--- Arquitectura/dist/app-bundle.js
+++ Arquitectura/dist/app-bundle.js
@@ -96,26 +96,72 @@ var Head = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Head.prototype.render = function () {
-        return (React.createElement("div", null,
-            React.createElement("form", null,
-                React.createElement("h1", null, "Renderizando en React!"),
-                React.createElement("label", { for: "id" }, "Last Name"),
-                React.createElement("input", { type: "text", id: "id", name: "idUser", placeholder: "Escriba la c\u00E9dula.." }),
-                React.createElement("label", { for: "name" }, "Nombre usuarios"),
-                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba el nombre..." }),
-                React.createElement("label", { for: "cuadrante" }, "Cuadrante"),
-                React.createElement("select", { id: "cuadrante", name: "cuadrante" },
-                    React.createElement("option", { value: "norte" }, "Norte"),
-                    React.createElement("option", { value: "sur" }, "Sur"),
-                    React.createElement("option", { value: "oriente" }, "Oriente"),
-                    React.createElement("option", { value: "occidente" }, "Occidente")),
-                React.createElement("label", { for: "subject" }, "Reporte"),
-                React.createElement("textarea", { id: "subject", name: "subject", placeholder: "Escriba el detalle del problema...", style: "height:200px" }),
-                React.createElement("input", { type: "submit", value: "Enviar" }))));
+        return (React.createElement("div", { class: "login-page" },
+            React.createElement("div", { class: "form" },
+                React.createElement("form", { class: "login-form" },
+                    React.createElement("input", { type: "text", placeholder: "C\u00E9dula" }),
+                    React.createElement("input", { type: "password", placeholder: "Contrase\u00F1a" }),
+                    React.createElement("button", null, "Entrar"),
+                    React.createElement("p", { class: "message" },
+                        "Crea una cuenta ",
+                        React.createElement("a", { href: "#" }, "aqu\u00ED"))))));
     };
     return Head;
 }(React.Component));
-ReactDOM.render(React.createElement(Head, null), document.getElementById('root'));
+var Registro = /** @class */ (function (_super) {
+    __extends(Registro, _super);
+    function Registro() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Registro.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement("form", null,
+                React.createElement("center", null,
+                    React.createElement("h1", null, "Registro de Usuarios")),
+                React.createElement("label", { for: "id" }, "C\u00E9dula del usuario"),
+                React.createElement("input", { type: "text", id: "id", name: "idUser", placeholder: "Escriba su c\u00E9dula de ciudadan\u00EDa.." }),
+                React.createElement("label", { for: "name" }, "Nombres y apellidos"),
+                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba su nombre completo..." }),
+                React.createElement("label", { for: "pas" }, "Contrase\u00F1a"),
+                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba la nueva contrase\u00F1a..." }),
+                React.createElement("label", { for: "name" }, "Email"),
+                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba su email..." }),
+                React.createElement("label", { for: "name" }, "Direcci\u00F3n"),
+                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba su direcci\u00F3n..." }),
+                React.createElement("label", { for: "name" }, "Tel\u00E9fono"),
+                React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba su tel\u00E9fono..." }),
+                React.createElement("center", null,
+                    React.createElement("input", { type: "submit", value: "Registrarse" })))));
+    };
+    return Registro;
+}(React.Component));
+var Reporte = /** @class */ (function (_super) {
+    __extends(Reporte, _super);
+    function Reporte() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Reporte.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement("h1", null, "Ingreso de Reporte"),
+            React.createElement("label", { for: "id" }, "C\u00E9dula del usuario"),
+            React.createElement("input", { type: "text", id: "id", name: "idUser", placeholder: "Escriba la c\u00E9dula.." }),
+            React.createElement("label", { for: "name" }, "Nombre usuario"),
+            React.createElement("input", { type: "text", id: "name", name: "names", placeholder: "Escriba el nombre..." }),
+            React.createElement("label", { for: "cuadrante" }, "Cuadrante"),
+            React.createElement("select", { id: "cuadrante", name: "cuadrante" },
+                React.createElement("option", { value: "norte" }, "Norte"),
+                React.createElement("option", { value: "sur" }, "Sur"),
+                React.createElement("option", { value: "oriente" }, "Oriente"),
+                React.createElement("option", { value: "occidente" }, "Occidente")),
+            React.createElement("label", { for: "subject" }, "Reporte"),
+            React.createElement("textarea", { id: "subject", name: "subject", placeholder: "Escriba el detalle del problema...", style: "height:200px" }),
+            React.createElement("input", { type: "submit", value: "Enviar" })));
+    };
+    return Reporte;
+}(React.Component));
+ReactDOM.render(React.createElement(Head, null), document.getElementById('log'));
+ReactDOM.render(React.createElement(Reporte, null), document.getElementById('rep'));
+ReactDOM.render(React.createElement(Registro, null), document.getElementById('reg'));
 
 
 /***/ }),
